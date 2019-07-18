9757c1d7a50ca3884a18013c36b6d83d2475f824
gudeck <guguzucoloto@gmail.com>
2019-07-18 02:54:16 -0300

    Resolução Desafio 5

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var qualquer = ["gustavo", 1, true, function () {}, undefined];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function funcao1(array) {
+  return array;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+funcao1(qualquer)[1]; // 1
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,21 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function funcao2(array, numero) {
+    return array[numero];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var queRecebe = ["string", 0, false, undefined, null];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+funcao1(queRecebe); // ["string", 0, false, undefined, null]
+
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +52,59 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nomeLivro){
+  var livros = {
+    "Programando em Python":{
+      quantidadePaginas: 16,
+      autor: "Livia",
+      editora: "Ifes"
+    },
+    "Kama Sutra":{
+      quantidadePaginas: 69,
+      autor: "Gustavo",
+      editora: "X"
+    },
+    "MongoDB NINJA":{
+      quantidadePaginas: 99,
+      autor: "Marlon",
+      editora: "?"
+    }
+  };
 
+  if(nomeLivro === undefined)
+    return livros;
+
+  return livros[nomeLivro];
+}
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
+/*  'Programando em Python': { quantidadePaginas: 16, autor: 'Livia', editora: 'Ifes' },
+    'Kama Sutra': { quantidadePaginas: 69, autor: 'Gustavo', editora: 'X' },
+    'MongoDB NINJA': { quantidadePaginas: 99, autor: 'Marlon', editora: '?' }
+ */
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+"O livro " + "Kama Sutra" + " tem " + book("Kama Sutra").quantidadePaginas + " páginas!";
+// 'O livro Kama Sutra tem 69 páginas!'
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+"O autor do livro " + "Kama Sutra" + " é " + book("Kama Sutra").autor + ".";
+// 'O autor do livro Kama Sutra é Gustavo.'
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+"O livro " + "Kama Sutra" + " foi publicado pela editora " + book("Kama Sutra").editora + ".";
+// 'O livro Kama Sutra foi publicado pela editora X.'
